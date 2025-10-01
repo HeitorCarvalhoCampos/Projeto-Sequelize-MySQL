b0193d21c7d0dd9e03d7595c04da66f4b18b89a4
HeitorCarvalhoCampos <heitor.campos@germinare.org.br>
2025-09-30 23:00:11 -0300

    Arquivo de query para gerar database

---
+++ banco_instrucoes.sql
@@ -0,0 +1,12 @@
+-- Mysql
+CREATE DATABASE nodesequelize CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE nodesequelize;
+
+-- PgAdmin
+CREATE DATABASE nodesequelize
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'pt_BR.utf8'
+    LC_CTYPE = 'pt_BR.utf8'
+    TEMPLATE = template0;
